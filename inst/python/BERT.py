0db694666d3b07a035f61ce2f2354cd301159609
creg <christophe.regouby@free.fr>
2020-03-01 16:19:19 +0100

    Expand golgotha::BERT into golgotha::transformer. Provides access to 10 model architectures Add dependence to testthat::

--- inst/python/BERT.py
+++ inst/python/BERT.py
@@ -1,9 +1,26 @@
-from transformers import BertModel, BertTokenizer, pipeline
+from transformers import *
 import torch
+# Transformers has a unified API
+# for 10 transformer architectures and 30 pretrained weights.
+#            Architecture| Model          | Tokenizer          | Pretrained weights shortcut
+MODELS = {    'BERT':      (BertModel,       BertTokenizer,       'bert-base-uncased'),
+              'GTP':       (OpenAIGPTModel,  OpenAIGPTTokenizer,  'openai-gpt'),
+              'GTP2':      (GPT2Model,       GPT2Tokenizer,       'gpt2'),
+              'CTRL':      (CTRLModel,       CTRLTokenizer,       'ctrl'),
+              'TransfoXL': (TransfoXLModel,  TransfoXLTokenizer,  'transfo-xl-wt103'),
+              'XLNet':     (XLNetModel,      XLNetTokenizer,      'xlnet-base-cased'),
+              'XLM':       (XLMModel,        XLMTokenizer,        'xlm-mlm-enfr-1024'),
+              'DistilBERT':(DistilBertModel, DistilBertTokenizer, 'distilbert-base-cased'),
+              'Roberta':   (RobertaModel,    RobertaTokenizer,    'roberta-base'),
+              'XLMRoberta':(XLMRobertaModel, XLMRobertaTokenizer, 'xlm-roberta-base')
+         }
+
 class Embedder():
-	def __init__(self, path = None):
-		self.model = BertModel.from_pretrained(path)
-		self.tokenizer = BertTokenizer.from_pretrained(path)
+	def __init__(self, path = None, architecture="BERT"):
+		model_class, tokenizer_class, pretrained_weights = MODELS[architecture]
+		# load pretrained model/tokenizer
+		self.model = model_class.from_pretrained(path)
+		self.tokenizer = tokenizer_class.from_pretrained(path)
 		self.nlp_feature_extraction = pipeline("feature-extraction", model = self.model, tokenizer = self.tokenizer)
 	def tokenize(self, text):
 		output = self.tokenizer.tokenize(text)
@@ -21,9 +38,11 @@ class Embedder():
 		output = output.numpy()
 		return(output)
 		
-def download(model_name = "bert-base-multilingual-uncased", path = None):
-	tokenizer = BertTokenizer.from_pretrained(pretrained_model_name_or_path = model_name)
-	model = BertModel.from_pretrained(pretrained_model_name_or_path = model_name, output_hidden_states = True, output_attentions = False)
+def download(model_name = "bert-base-multilingual-uncased", path = None, architecture="BERT"):
+	model_class, tokenizer_class, pretrained_weights = MODELS[architecture]
+	# load pretrained model/tokenizer
+	tokenizer = tokenizer_class.from_pretrained(pretrained_model_name_or_path = model_name)
+	model = model_class.from_pretrained(pretrained_model_name_or_path = model_name, output_hidden_states = True, output_attentions = False)
 	tokenizer.save_pretrained(path)
 	model.save_pretrained(path)
 	return(path)
