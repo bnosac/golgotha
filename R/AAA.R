ff9c612e2828a726f86c44299a0332305ca0939b
Jan Wijffels <jwijffels@bnosac.be>
2020-03-14 21:22:20 +0100

    Fix for #4

--- R/AAA.R
+++ R/AAA.R
@@ -1,13 +1,19 @@
 
 
-nlp <- new.env()
+local <- new.env()
 
 .onLoad <- function(libname, pkgname) {
   packageStartupMessage("- Connecting/Setting up Python using reticulate")
   reticulate::configure_environment(pkgname)
   packageStartupMessage("- Loading golgotha BERT code")
-  pyscript <- system.file(package = "golgotha", "python", "BERT.py")
-  source_python(pyscript, envir = nlp, convert = TRUE)
+
+  oldwd <- getwd()
+  on.exit(setwd(oldwd))
+  setwd(system.file(package = "golgotha", "python"))
+  nlp <- import("BERT", convert = TRUE, delay_load = TRUE)
+  assign("nlp", value = nlp, envir = parent.env(local))
+  #pyscript <- system.file(package = "golgotha", "python", "BERT.py")
+  #source_python(pyscript, envir = nlp, convert = TRUE)
 }
 
 known_architectures <- c("BERT", "GPT", "GPT-2", "CTRL", "Transformer-XL", "XLNet", "XLM", "DistilBERT", "RoBERTa", "XLM-RoBERTa",
