840482f43f97ae4b5eb566e2fbcdfd28ba64ea80
Jan Wijffels <jwijffels@bnosac.be>
2020-05-13 17:57:24 +0200

    extend unit testing on travis

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,6 +1,23 @@
+unlink_model_directory <- function(){
+  d <- system.file(package = "golgotha", "models")
+  d <- setdiff(list.dirs(d), d)
+  unlink(d, recursive = TRUE)
+}
+
 if(requireNamespace("tinytest", quietly = TRUE)){
   if(tinytest::at_home()){
     library(golgotha)
     tinytest::test_package("golgotha")
+    unlink_model_directory()
+  }
+  if(identical(Sys.getenv("TT_AT_CI"), "TRUE")){
+    library(golgotha)
+    testdir <- system.file("tinytest", package = "golgotha")
+    tinytest::run_test_file(file = file.path(testdir, "test-download.R"))
+    unlink_model_directory()
+    tinytest::run_test_file(file = file.path(testdir, "test-transformer-default.R"))
+    unlink_model_directory()
+    tinytest::run_test_file(file = file.path(testdir, "test-distilbert.R"))
+    unlink_model_directory()
   }
 }
